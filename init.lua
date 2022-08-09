fe2271b8f2c97480ede24ab95d7288973258b765
Dalton Dick <daltondick2010@gmail.com>
2022-08-09 17:36:27 -0500

    Configure blamer

--- init.lua
+++ init.lua
@@ -2,4 +2,9 @@
 -- load your options globals, autocmds here or anything .__.
 -- you can even override default options here (core/options.lua)
 
+-- ["APZelos/blamer.nvim"]
 vim.g.blamer_enabled = 1
+vim.g.blamer_template = '<author>, <author-time> • <summary>'
+vim.g.blamer_date_format = '%m/%d/%y %I:%M %p'
+vim.g.blamer_relative_time = 1
+
