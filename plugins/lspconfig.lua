ac4a41cc37a815c68db098dd0c703ff2f6b6f661
Dalton Dick <daltondick2010@gmail.com>
2022-10-10 19:46:08 -0500

    Configure sumneko_lua

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -15,7 +15,6 @@ local servers = {
   "jsonls",
   "omnisharp",
   "solargraph",
-  "sumneko_lua",
   "tsserver",
 }
 
@@ -28,8 +27,34 @@ end
 
 DATA_PATH = vim.fn.stdpath('data')
 
-require'lspconfig'.elixirls.setup {
+lspconfig.elixirls.setup {
   cmd = { DATA_PATH.."/mason/bin/elixir-ls" },
   on_attach = on_attach,
   capabilities = capabilities,
+}
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
